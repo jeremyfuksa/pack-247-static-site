a421517bae3c2a6ff2475fae7f51b331575ce583
Jeremy Fuksa <hello@orangefla.me>
2018-08-17 11:12:50 -0500

    Fixing Slack API token problem.
    Signed-off-by: Jeremy Fuksa <hello@orangefla.me>

--- slack/config.php
+++ slack/config.php
@@ -6,4 +6,4 @@ define('TEAM_SUB_DOMAIN','pack247');
 
 // API TOKEN.
 // ref: https://api.slack.com/web
-define('API_TOKEN','xoxp-26889369141-26882019462-74558839989-03e7166636');
+define('API_TOKEN','API_TOKEN_REDACTED');
